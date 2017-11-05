11910ae687083530ce044ed5fb8843990fa258ce
Ningfei Wang <wangningfei7@gmail.com>
2017-11-05 16:43:07 -0500

    choco install exe and control

--- Choco/choco_ins.py
+++ Choco/choco_ins.py
@@ -9,27 +9,47 @@ import os
 from selenium import webdriver
 import time
 
+os.system('z:')
 url = "http://www.baidu.com"
 f = open("choco_install_ins.txt")
 lines = f.readlines()
 
-for line in lines:
-	print line
-	
-	a = os.system(line)
-	# time.sleep(15)
-	# print k[0].split('\n')[0]
-	if a == 0:
-		print "install success"
-		browser = webdriver.Chrome()
-		browser.get(url)
-		browser.quit()	
-	else:
-		print "install fail"
+# for line in lines:
+line = lines[0]
+print line
 
+a = os.system(line)
+if a == 0:
+	print "install success"
+	browser = webdriver.Chrome()
+	browser.get(url)
+	browser.quit()	
 	log = open('log.txt', 'a')
 	log.write(line)
-	log.write(str(a))
+	log.write('success')
 	log.write('\n')
 	log.close()
-	
+else:
+	print "install fail"
+	log = open('log.txt', 'a')
+	log.write(line)
+	log.write('unsuccess')
+	log.write('\n')
+	log.close()
+
+f1 = open('test_finish.txt','w+')
+# f = open("choco_app_name.txt",'r')
+# lines1 = f1.readlines()
+if 'hadoop' in line:
+	# f.write(lines1[0])
+	f1.write('finished')
+	f1.close()
+else:
+	f1.write('no')
+	f1.close()
+	# break
+f2 = open('test_finish_ins.txt','w+')
+			# lines_f2 = f2.readlines()
+f2.write('1')
+f2.close()
+
